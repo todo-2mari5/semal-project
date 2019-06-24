f10936d6ce1c5fe449702c890c73c80ef3f4b09d
Oishi Azi <ha1ma2chi3@yahoo.co.jp>
2019-06-24 16:37:15 +0900

    yatto chuukan chiten

--- myapp/forms.py
+++ myapp/forms.py
@@ -3,16 +3,10 @@ from .models import Post
 
 
 class PostForm(forms.ModelForm):
-    file = forms.FileField(required=False)
     file = forms.FileField(
         label='',
         required=False
     )
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field in self.fields.values():
-            field.widget.attrs['class'] = 'form-control'
 
     class Meta:
         model = Post
--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def post_list(req):
-    posts = Post.objects.filter(publish_date__lte=timezone.now()).order_by('-publish_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(req, 'myapp/post_list.html', {"posts": posts})
 
 def post_detail(req, pk):
@@ -16,10 +17,25 @@ def post_new(req):
         form = PostForm(req.POST, req.FILES)
         if form.is_valid():
             post = form.save(commit=False)
-            post.author = request.user
+            post.author = req.user
             post.published_date = timezone.now()
+            post.image = form.cleaned_data['image']
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
-    return render(request, 'myapp/post_edit.html', {'form': form})
+    return render(req, 'myapp/post_edit.html', {'form': form})
+
+def post_edit(req, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if req.method == "POST":
+        form = PostForm(req.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = req.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(req, 'myapp/post_edit.html', {'form': form})
