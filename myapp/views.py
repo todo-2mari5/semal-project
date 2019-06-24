cc2f3cc18cf5f7fb236bacc8a10cb464bf19b9ce
Oishi Azi <ha1ma2chi3@yahoo.co.jp>
2019-06-24 14:34:06 +0900

    template made finish

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
+def post_list(req):
+    posts = Post.objects.filter(publish_date__lte=timezone.now()).order_by('-publish_date')
+    return render(req, 'myapp/post_list.html', {"posts":posts})
